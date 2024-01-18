7c9f623a8fcdb68198257bb0533602e2571308d6
Nicolas Cerardi <nicolas.cerardi@gmail.com>
2024-01-18 16:42:31 +0100

    factoring some code

--- xrayflux/xraytables.py
+++ xrayflux/xraytables.py
@@ -10,7 +10,85 @@ from scipy.interpolate import interp1d, interp2d
 from xraytables.fitsutils import mwrfits
 import matplotlib.pyplot as plt
 
-def make_SLT(outfile=None,
+def compute_flux(range_E=[0.5, 2.0], n_E=10, 
+             T=1, 
+             z=0., 
+             abund=0.3,
+             idx_start_abund=1,
+             elements=None,
+             rmf=None, 
+             arf=None,
+             eebrems=False,
+             verbose=0,
+             plots=False):
+    
+    ebins_in, ebins_out, ebins_in_m, ebins_out_m, rmfmatrix, arf, abres, elements = \
+                                prepare_session_rmf_arf_elts(range_E, n_E, rmf, arf)
+    
+    tmp_sess = pyatomdb.spectrum.CIESession(elements=elements)
+    tmp_sess.set_eebrems(eebrems)
+    if elements[0]!=1:
+        # it's all metals anyway
+        tmp_sess.set_abund(elements,abund)
+        abres[:] = abund
+    elif len(elements)>1:
+        # whatever you ask
+        tmp_sess.set_abund(elements[idx_start_abund:],abund)
+        abres[idx_start_abund:] = abund
+    # other case is just H alone, don't change its abundancy
+    if plots:
+        fig, axs = plt.subplots(1, 2, figsize=[15,10])
+    tmp_ebins = ebins_in*(1+z) 
+    tmp_sess.set_response(tmp_ebins, raw=True)
+
+    # returns f/ne so in ph cm^3 s-1 bin-1
+    tmp_spec = tmp_sess.return_spectrum(kT)
+    if plots: axs[0].loglog(mid_vals(ebins_in), tmp_spec, label=('%.1f keV'%kT))
+    # res in ph cm^3 s-1, or ph cm^5 s-1 if rmf and arf are used.
+    if verbose>0 : print(tmp_spec.shape, rmfmatrix.shape)
+    #apply rmf then arf
+    if rmf is not None: tmp_spec = np.matmul(tmp_spec,rmfmatrix)
+    tmp_spec *= arf #np.ones(ebins_out_m.shape) #
+    if plots: axs[1].loglog(mid_vals(ebins_out), tmp_spec, label=('%.1f keV'%kT))
+    #integrate spectrum, apply l.o.s. dilatation and store it at the right (z,kT) index
+    res = tmp_spec.sum()/(1+z) 
+    if plots:
+        axs[1].annotate('z=%.1f'%z, (0.2,0.2), xycoords='axes fraction', size=15, color='k')
+        axs[1].legend()
+        axs[0].set_xlim([range_E[0], range_E[1]])
+        axs[1].set_xlabel('keV')
+        axs[0].set_ylabel('ph cm^3 s^-1')
+        axs[1].set_ylabel('ph cm^5 s^-1')
+    return res
+    
+    
+def prepare_session_rmf_arf_elts(range_E=[0.5, 2.0], n_E=10, rmf, arf):
+    # ebins in and out are because of the rmf. ebins in must be larger as some photons outside 
+    # the ebins_out may be falsely detected inside ebins_out... we have to take account of them.
+    # boundaries
+    ebins_out = np.linspace(range_E[0],range_E[1],n_E)
+    ebins_in = ebins_out.copy()
+    # central values of bins
+    ebins_out_m = mid_vals(ebins_out)
+    ebins_in_m = mid_vals(ebins_in)
+    if verbose>0:
+        print(ebins_out)
+        print(ebins_in)
+    if rmf is not None:
+        assert arf is not None
+        ebins_in = np.linspace(range_E[0]*0.5,range_E[1]*2,n_E*4) #only useful in rmf is not None
+        ebins_in_m = mid_vals(ebins_in)
+        # load and prepare the aeff, arf, and their E range.
+        rmfmatrix, arf = load_rmf_arf(rmf, arf, mid_vals(ebins_in), mid_vals(ebins_out))
+    else : 
+        # just a dummy aeff
+        arf = np.ones(ebins_out_m.shape)
+    # abundancies and elements
+    abres = np.ones(np.array(elements).shape)
+    elements = elements if elements is not None else range(1, 3)
+    return ebins_in, ebins_out, ebins_in_m, ebins_out_m, rmf, arf, abres, elements
+
+def make_fluxtable(outfile=None,
              range_E=[0.5, 2.0], n_E=10, 
              range_T=[0.05, 50], n_T=10, 
              range_z=[0.0, 3.0], n_z=10, 
@@ -19,12 +97,11 @@ def make_SLT(outfile=None,
              elements=None,
              rmf=None, 
              arf=None,
-             unit="photon",
              eebrems=False,
              verbose=0,
              plots=False):
     """
-    Generate SLT (Simulated Light Travel) data.
+    Generate tabulated xray flux data.
 
     Parameters:
     - outfile (str): Output file path.
@@ -39,7 +116,6 @@ def make_SLT(outfile=None,
     - elements (list): List of elements.
     - rmf (str): RMF file path.
     - arf (str): ARF file path.
-    - unit (str): Unit type.
     - eebrems (bool): Include electron-electron bremsstrahlung.
     - verbose (int): Verbosity level.
     - plots (bool): Generate plots.
@@ -47,48 +123,23 @@ def make_SLT(outfile=None,
     Returns:
     - tuple: Result, redshift bins, temperature bins, elements, abundance results.
     """
-    # ebins in and out are because of the rmf. ebins in must be larger as some photons outside 
-    # the ebins_out may be falsely detected inside ebins_out... we have to take account of them.
-    # boundaries
-    ebins_out = np.linspace(range_E[0],range_E[1],n_E)
-    ebins_in = ebins_out.copy()
-    
-    # central values of bins
-    ebins_out_m = mid_vals(ebins_out)
-    ebins_in_m = mid_vals(ebins_in)
-    
     # redshift and Temperature
     zbins = np.linspace(range_z[0],range_z[1],n_z)
     Tbins = np.logspace(np.log10(range_T[0]),np.log10(range_T[1]),n_T)
     
     if verbose>0:
-        print(ebins_out)
-        print(ebins_in)
         print(zbins)
         print(Tbins)
-    
     res = np.zeros((n_z, n_T))
-    abres = np.ones(np.array(elements).shape)
-    elements = elements if elements is not None else range(1, 3)
     
-    if rmf is not None:
-        assert arf is not None
-        ebins_in = np.linspace(range_E[0]*0.5,range_E[1]*2,n_E*4) #only useful in rmf is not None
-        ebins_in_m = mid_vals(ebins_in)
-        # load and prepare the aeff, arf, and their E range.
-        rmfmatrix, arf = load_rmf_arf(rmf, arf, mid_vals(ebins_in), mid_vals(ebins_out))
-        #print(rmfmatrix.shape, arf.shape)
-        # interpolate aeff from its original E range to ebins
-        #aeff_ebins = interp1d(specbins[:-1], aeff)(ebins[:-1])
-    else : 
-        # just a dummy aeff
-        arf = np.ones(ebins_out_m.shape)
-    
+    ebins_in, ebins_out, ebins_in_m, ebins_out_m, rmfmatrix, arf, abres, elements = \
+                                prepare_session_rmf_arf_elts(range_E, n_E, rmf, arf)
     
     if plots:
         fig, axs = plt.subplots(2, n_z, figsize=[15,10])
         [[a.set_xscale('log') for a in ax] for ax in axs]
         [[a.set_yscale('log') for a in ax] for ax in axs]
+        
     with tqdm(total=n_z * n_T) as pbar:
         tmp_sess = pyatomdb.spectrum.CIESession(elements=elements)
         tmp_sess.set_eebrems(eebrems)
